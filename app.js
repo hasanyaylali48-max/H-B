f3fe8baf97c5ebb55df63db34781ab198e9c840c
hasanyaylali48-max <hasanyaylali48@gmail.com>
2026-04-05 23:40:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// Kadın sesli asistan - hoşgeldiniz mesajı
 function speakWelcome() {
   const msg = new SpeechSynthesisUtterance("Hoşgeldiniz, H ve B uygulamasına giriş yapıyorsunuz.");
   msg.lang = "tr-TR";
-  msg.pitch = 1.2; // kadın sesi için daha ince ton
-  msg.rate = 1;    // normal hız
+  msg.pitch = 1.2;
+  msg.rate = 1;
   window.speechSynthesis.speak(msg);
 }
 
-// Panellere geçiş
 function goToPanels() {
   // Sesli asistanı butona basınca çalıştır
   speakWelcome();
